336bd8c7c13cf62ac85144906c6129776e555ae1
tundelola <foladayo@aol.com>
2016-12-27 16:05:18 -0500

    first commit

---
+++ arrayPrac/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array3 = {5, 6, 1, 0, -1, 3, 4, 5 };
+            int min = 5;
+            int max;
+
+            for ( min = 5; min> array3.Length; min--)
+            {
+                Console.WriteLine(array3[min])
+                    Array.
+            }
+        }
+    }
+}
